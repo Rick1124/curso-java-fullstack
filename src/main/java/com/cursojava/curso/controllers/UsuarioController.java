743421915acfeee49c3dd083cebc8a410592e7e2
ricardo <rimasice@hotmail.com>
2022-11-23 04:02:06 -0600

    03:06:53

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,43 +1,27 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "usuarios")
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
+    public void registrarUsuario(@RequestBody Usuario usuario){
+        usuarioDao.registrar(usuario);
+    }
+
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
     public List <Usuario> getUsuarios(){
-        List <Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(235L);
-        usuario.setNombre("Lucas");
-        usuario.setApellido("Moy");
-        usuario.setEmail("lucasmoy@hotmail.com");
-        usuario.setTelefono("234234234");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(365L);
-        usuario2.setNombre("Maria");
-        usuario2.setApellido("Gonzales");
-        usuario2.setEmail("maria.gon@hotmail.com");
-        usuario2.setTelefono("365365365");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(425L);
-        usuario3.setNombre("Gabriel");
-        usuario3.setApellido("Aliaga");
-        usuario3.setEmail("alaiga124@hotmail.com");
-        usuario3.setTelefono("234234234");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
     @RequestMapping(value = "usuario1")
@@ -50,14 +34,9 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuario2")
-    public Usuario eliminar(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Lucas");
-        usuario.setApellido("Moy");
-        usuario.setEmail("lucasmoy@hotmail.com");
-        usuario.setTelefono("234234234");
-        return usuario;
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id){
+        usuarioDao.eliminar(id);
     }
 
     @RequestMapping(value = "usuario3")
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 async function cargarUsuarios() {
 
-  const request = await fetch('usuarios', {
+  const request = await fetch('api/usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -15,20 +15,37 @@ async function cargarUsuarios() {
   });
   const usuarios = await request.json();
 
-  console.log(usuarios);
+let listadoHtml = '';
+  for (let usuario of usuarios) {
+    let botonEliminar = '<a href="#" onclick="eliminarUsuario('+usuario.id+')" class="btn btn-danger btn-circle btn-sm">' + '<i class=\"fas fa-trash\"></i>' + '</a>';
 
-  let usuario = '                                        <tr>\n' +
-      '                                            <td>123</td>\n' +
-      '                                            <td>Prueba Moy</td>\n' +
-      '                                            <td>lucasmoy@hotmail.com</td>\n' +
-      '                                            <td>23423423</td>\n' +
-      '                                            <td>\n' +
-      '                                                <a href="#" class="btn btn-danger btn-circle btn-sm">\n' +
-      '                                                    <i class="fas fa-trash"></i>\n' +
-      '                                                </a>\n' +
-      '                                            </td>\n' +
-      '                                        </tr>'
+    let telefonoTexto = usuario.telefono == null ? '-' :usuario.telefono;
+    let usuarioHtml = '<tr>\n' +
+                        '<td>'+usuario.id+'</td>' +
+                        '<td>'+usuario.nombre + ' ' + usuario.apellido+'</td>' +
+                        '<td>'+usuario.email+'</td>' +
+                        '<td>'+telefonoTexto+'</td>' +
+                        '<td>' + botonEliminar + '</td>' +
+                      '</tr>'
+    listadoHtml += usuarioHtml;
 
-  document.querySelector("#usuarios tbody").outerHTML = usuario;
+  }
+  document.querySelector("#usuarios tbody").outerHTML = listadoHtml;
+}
 
+async function eliminarUsuario(id) {
+
+  if (!confirm("¿Desea eliminar éste usuario?")){
+    return;
+  }
+
+
+  const request = await fetch('api/usuarios/' + id, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  });
+  location.reload();
 }
